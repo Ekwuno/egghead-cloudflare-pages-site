7194c80572d5b83fc3b09cd5449ecb7d92c6af63
Ekwuno <obinnacodes@gmail.com>
2021-10-08 15:11:13 +0100

    add serverless fucntion

--- components/form.js
+++ components/form.js
@@ -12,6 +12,7 @@ export default function Form() {
 		>
 			{/* <p> Contact Us </p> */}
 			<form
+				action="https://airtable-worker.obinnacodes.workers.dev"
 				method="POST"
 				className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
 			>
